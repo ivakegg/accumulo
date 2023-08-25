0e2397521972e0077b935f58bf4957645aae13bc
Ed Coleman <edcoleman@apache.org>
2023-08-25 22:11:51 +0000

    checkstyle suggestions

--- server/tserver/src/main/java/org/apache/accumulo/tserver/tablet/DatafileTransactionLog.java
+++ server/tserver/src/main/java/org/apache/accumulo/tserver/tablet/DatafileTransactionLog.java
@@ -18,9 +18,6 @@
  */
 package org.apache.accumulo.tserver.tablet;
 
-import org.apache.accumulo.core.dataImpl.KeyExtent;
-import org.apache.accumulo.core.metadata.StoredTabletFile;
-
 import java.time.Instant;
 import java.util.Collections;
 import java.util.Date;
@@ -30,74 +27,78 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
+import org.apache.accumulo.core.dataImpl.KeyExtent;
+import org.apache.accumulo.core.metadata.StoredTabletFile;
+
 public class DatafileTransactionLog {
-    private final KeyExtent extent;
-    private Set<StoredTabletFile> initialFiles = new HashSet<>();
-    private long initialTs = System.currentTimeMillis();
-    private List<DatafileTransaction> tabletLogs = Collections.synchronizedList(new LinkedList<>());
-    private final int maxSize;
+  private final KeyExtent extent;
+  private final Set<StoredTabletFile> initialFiles = new HashSet<>();
+  private final long initialTs = System.currentTimeMillis();
+  private final List<DatafileTransaction> tabletLogs =
+      Collections.synchronizedList(new LinkedList<>());
+  private final int maxSize;
 
-    public DatafileTransactionLog(KeyExtent extent, Set<StoredTabletFile> initialFiles, int maxSize) {
-        this.extent = extent;
-        this.maxSize = maxSize;
-        this.initialFiles.addAll(initialFiles);
-    }
+  public DatafileTransactionLog(KeyExtent extent, Set<StoredTabletFile> initialFiles, int maxSize) {
+    this.extent = extent;
+    this.maxSize = maxSize;
+    this.initialFiles.addAll(initialFiles);
+  }
 
-    public Date getInitialDate() {
-        return Date.from(Instant.ofEpochSecond(initialTs));
-    }
+  public Date getInitialDate() {
+    return Date.from(Instant.ofEpochSecond(initialTs));
+  }
 
-    private void checkSize() {
-        flush(maxSize);
-    }
+  private void checkSize() {
+    flush(maxSize);
+  }
 
-    public void flush(int size) {
-        while (tabletLogs.size() > size) {
-            applyTransaction();
-        }
+  public void flush(int size) {
+    while (tabletLogs.size() > size) {
+      applyTransaction();
     }
+  }
 
-    private void applyTransaction() {
-        // synchronize to keep both the remove and apply atomic
-        synchronized(tabletLogs) {
-            tabletLogs.remove(0).apply(initialFiles);
-        }
+  private void applyTransaction() {
+    // synchronize to keep both the remove and apply atomic
+    synchronized (tabletLogs) {
+      tabletLogs.remove(0).apply(initialFiles);
     }
+  }
 
-    public Set<StoredTabletFile> getExpectedFiles() {
-        Set<StoredTabletFile> files = new HashSet<>();
-        // synchronize to ensure consistency between initialFiles and the logs
-        synchronized(tabletLogs) {
-            files.addAll(initialFiles);
-            tabletLogs.stream().forEach(t -> t.apply(files));
-        }
-        return files;
+  public Set<StoredTabletFile> getExpectedFiles() {
+    Set<StoredTabletFile> files;
+    // synchronize to ensure consistency between initialFiles and the logs
+    synchronized (tabletLogs) {
+      files = new HashSet<>(initialFiles);
+      tabletLogs.forEach(t -> t.apply(files));
     }
+    return files;
+  }
 
-    public void compacted(Set<StoredTabletFile> files, Optional<StoredTabletFile> output) {
-        tabletLogs.add(new DatafileTransaction.Compacted(files, output));
-        checkSize();
-    }
+  public void compacted(Set<StoredTabletFile> files, Optional<StoredTabletFile> output) {
+    tabletLogs.add(new DatafileTransaction.Compacted(files, output));
+    checkSize();
+  }
 
-    public void flushed(Optional<StoredTabletFile> newDatafile) {
-        tabletLogs.add(new DatafileTransaction.Flushed(newDatafile));
-        checkSize();
-    }
+  public void flushed(Optional<StoredTabletFile> newDatafile) {
+    tabletLogs.add(new DatafileTransaction.Flushed(newDatafile));
+    checkSize();
+  }
 
-    public void bulkImported(StoredTabletFile file) {
-        tabletLogs.add(new DatafileTransaction.BulkImported(file));
-        checkSize();
-    }
+  public void bulkImported(StoredTabletFile file) {
+    tabletLogs.add(new DatafileTransaction.BulkImported(file));
+    checkSize();
+  }
 
-    @Override
-    public String toString() {
-        StringBuilder builder = new StringBuilder();
-        synchronized(tabletLogs) {
-            builder.append(String.format("%s: Initial files : %s\n", getInitialDate(), initialFiles));
-            tabletLogs.stream().forEach(t -> builder.append(t).append('\n'));
-            builder.append("Final files: ").append(getExpectedFiles());
-        }
-        return builder.toString();
+  @Override
+  public String toString() {
+    StringBuilder builder = new StringBuilder();
+    synchronized (tabletLogs) {
+      builder.append(String.format("%s: Initial files : %s\n", getInitialDate(), initialFiles));
+      tabletLogs.forEach(t -> builder.append(t).append('\n'));
+      builder.append("Final files: ").append(getExpectedFiles());
     }
+    return builder.toString();
+  }
 
 }
